cedd15cbbf77f122eb50abc8956c5ca5737a7e80
Imindu-J <upekshaimindu@gmail.com>
2026-07-09 11:31:17 +0530

    Initial commit: Spring Boot scaffolding, Docker COmpose, Health endpoint

---
+++ src/main/java/edu/carpool/carpool_rideshare/controller/HealthController.java
@@ -0,0 +1,13 @@
+package edu.carpool.carpool_rideshare.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/api/health")
+    public String health(){
+        return "Ok";
+    }
+}
---
+++ src/test/java/edu/carpool/carpool_rideshare/CarpoolRideshareApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.carpool.carpool_rideshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolRideshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
